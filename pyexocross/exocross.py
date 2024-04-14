a222f9cd44d126b2f5fd28881cc387ad928f2604
DGonzalezPicos <dariogonzalezpicos@gmail.com>
2024-04-14 15:55:44 +0200

    Add PT-grid cross-section calculation

--- pyexocross/exocross.py
+++ pyexocross/exocross.py
@@ -79,6 +79,10 @@ class ExoCross:
         self.label = f"{self.info['Iso-slug']}__{self.info['Isotopologue dataset name']}"
         self.mass = float(self.info['Isotopologue mass (Da) and (kg)'].split(' ')[0])
         return self        
+    
+    def set_broadening_params(self):
+        # TODO: implement this
+        pass
         
     def generate_inp(self,
                      temperature,
@@ -88,22 +92,19 @@ class ExoCross:
         if not hasattr(self, 'files'):
             self.check_input()
             
-        # pffile = self.pf.name
-        # states = self.states.name
-        # transitions = [t.name for t in self.trans]
-        # use absolute paths instead
+        # use absolute paths 
         pffile = str(self.pf)
         states = str(self.states)
         transitions = [str(t) for t in self.trans]
         
-        # out_file = f"{self.name}_{temperature:f}K_{pressure:f}bar.out"
-
-        species_str = '\n 0 gamma 0.06 n 0.5 t0 296 ratio 1.\nend'
-
         # write the .inp file
         inp_file = self.input / f'{self.name}_input_{temperature:.1f}_{pressure:.0e}.inp'
         out_file = self.tmp / inp_file.name.replace('.inp', '.out')
         
+        # default broadening
+        # TODO: enable user to change this in 'set_broadening_params()'
+        species_str = '\n 0 gamma 0.06 n 0.5 t0 296 ratio 1.\nend'
+
         with open(inp_file, 'w') as f:
             f.write(f'Nprocs {Nprocs}\n')
             f.write('absorption\n')
@@ -124,7 +125,6 @@ class ExoCross:
                     
             f.write('end\n')
             f.write(f'species {species_str}')
-            # close the file
             f.close()
         self.logger.info(f' Wrote {inp_file}')
         
@@ -141,7 +141,7 @@ class ExoCross:
         assert files_path.exists(), f'Path {files_path} does not exist'
         
         if PTpaths.exists() and not overwrite:
-            self.logger.info(f'{PTpaths} already exists. Use overwrite=True to overwrite')
+            self.logger.info(f' {PTpaths} already exists. Use overwrite=True to overwrite')
             return self
             
         
@@ -164,7 +164,7 @@ class ExoCross:
         self.rows = np.array(self.rows)[idx]
         # save the PT paths to a file with extension 6 digits for the first two columns
         np.savetxt(PTpaths, self.rows, fmt=['%10s', '%10s', '%s'])
-        self.logger.info(f'Wrote {PTpaths}')
+        self.logger.info(f' Wrote {PTpaths}')
         return self
     
     
@@ -172,42 +172,68 @@ class ExoCross:
         
         # create input file
         inp_file, out_file = self.generate_inp(temperature, pressure, Nprocs)
-        print(out_file)
-        # run exocross
         input_file = str(self.input / inp_file)
         output_file = str(self.tmp / out_file)
         self.logger.info(f'Running exocross with input file: {input_file}')
-        self.logger.info(f'Output will be saved to: {output_file}')
-        # command = [f"{self.path_exocross}/xcross.exe",
-        #            f"< {input_file}",
-        #            f"> {output_file}"]
-        command = [f"{self.path_exocross}/xcross.exe"]
+        self.logger.info(f' Output will be saved to: {output_file}')
+        
+        command = f"{self.path_exocross}/xcross.exe"
 
-        arguments = ["<", input_file, ">", output_file]
-
-        # call the command and send output to the logger
-        # print(command+arguments)
-        # self.logger.info(f'Running: {command}')
-        # pass input and output to the command
-        # sp.run(command+arguments)
-        result = sp.run(["sh", "-c", f"{command[0]} < {input_file} > {output_file}"], capture_output=True)
+        # call the command
+        result = sp.run(["sh", "-c", f"{command} < {input_file} > {output_file}"], capture_output=True)
         if result.returncode != 0:
-            print("Error running command!")
             print(result.stderr.decode("utf-8"))  # Decode and print error messages
-        # result = sp.run(command, capture_output=True, stdin=sp.PIPE)
-        # # Check for successful execution (return code 0)
-        # if result.returncode == 0:
-        #     return result.returncode, result.stdout.decode()
-        # else:
-        #     raise RuntimeError(f"xcross.exe exited with non-zero code: {result.returncode}")
         
-        print('done')
-        self.logger.info(f'Finished running {command}')
-        # return the output file
-        
+        self.logger.info(f' Finished running {command}')        
         return self
     
+    def xcross_grid(self, Nprocs=4):
+        
+        assert hasattr(self, 'T_grid'), 'No temperature grid found'
+        for T, P in zip(self.T_grid, self.P_grid):
+            self.logger.info(f' Running exocross for T={T} K, P={P} bar')
+            self.xcross(T, P, Nprocs)
+        
+        self.rebin_to_pRT()
+        return self
     
+    def rebin_to_pRT(self):
+        """ Rebin the cross sections to the pRT grid
+        
+        Adapted from pRT documentation"""
+        
+        files = sorted(self.tmp.glob('*.out.xsec'))
+        self.logger.info(f' Found {len(files)} files in tmp folder')
+        
+        wave_pRT = np.genfromtxt('wlen_petitRADTRANS.dat')
+        for i, file_i in enumerate(files):
+            self.logger.info(f' Rebinning {file_i} ({i}/{len(files)})')
+            P_str = file_i.split('bar')[-2].split('_')[-1]
+            # print(f'P_str = {P_str}')
+            self.logger.debug(f'P_str = {P_str}')
+            P = float(P_str)
+            
+            # read the cross sections
+            dat = np.genfromtxt(file_i)
+            wavelength = 1./dat[:,0]
+            sigma = dat[:,1]
+
+            # Invert them to go from a accending wavenumber ordering
+            # to an accending wavelength ordering.
+            wavelength = wavelength[::-1]
+            sigma = sigma[::-1]
+            
+            # interpolate to the pRT grid
+            sig_interpolated_petit = np.interp(wave_pRT, wavelength, sigma)
+            
+            T = float(file_i.split('K_')[-2].split('_')[-1])
+            
+            new_file_i = f'sigma_06_{T:.0f}.K_{P:.6f}bar.dat'
+            
+            # save rebinned cross sections
+            np.savetxt(new_file_i, np.column_stack((wave_pRT, sig_interpolated_petit)))
+            self.logger.info(f' Wrote {new_file_i}')
+        return self
 
         
 if __name__ == '__main__':
@@ -216,8 +242,8 @@ if __name__ == '__main__':
     exo.check_input()
     exo.read_definitions_file()
     # exo.generate_PTpaths('/home/dario/phd/pRT_input/input_data/opacities/lines/line_by_line/CO2_main_iso')
-    # exo.load_PT_grid()
+    exo.load_PT_grid(file='PT_grids/PTpaths_test.ls')
     # inp_file, out_file =exo.generate_inp(temperature=300, pressure=1e-5)
-    exo.xcross(temperature=300, pressure=1e-5, Nprocs=1)
+    # exo.xcross(temperature=300, pressure=1e-5, Nprocs=1)
     
     
